7b206c58c36efe3b18b0f5487058b214f5fdbafe
Peter Pfleiderer <peter.pfleiderer@posteo.de>
2025-07-31 11:07:26 +0200

    finishing some runs

--- data_extraction/extract_all.sh
+++ data_extraction/extract_all.sh
@@ -4,15 +4,14 @@ cd ../../REA_with_CESM2/data_extractor
 
 ID=$1
 
-python ../../REA_with_CESM2/data_extractor/extract.py --realm atm --h_identifier h1 --variable TREFHT --preprocessing regional_average_TREFHT --project_path ../../REA_heat_wEU_JJA/ --experiment_identifiers $ID
-
-echo "var_name='SHFLX'" > current_var_name.py
 python ../../REA_with_CESM2/data_extractor/extract.py --realm atm --h_identifier h2 --variable SHFLX --preprocessing regional_average --project_path ../../REA_heat_wEU_JJA/ --experiment_identifiers $ID
 
-echo "var_name='LHFLX'" > current_var_name.py
 python ../../REA_with_CESM2/data_extractor/extract.py --realm atm --h_identifier h2 --variable LHFLX --preprocessing regional_average --project_path ../../REA_heat_wEU_JJA/ --experiment_identifiers $ID
 
-echo "var_name='SOILWATER_10CM'" > current_var_name.py
 python ../../REA_with_CESM2/data_extractor/extract.py --realm lnd --h_identifier h7 --variable SOILWATER_10CM --preprocessing regional_average --project_path ../../REA_heat_wEU_JJA/ --experiment_identifiers $ID
 
 python ../../REA_with_CESM2/data_extractor/extract.py --realm atm --h_identifier h1 --variable pr --preprocessing regional_average_precip --project_path ../../REA_heat_wEU_JJA/ --experiment_identifiers $ID
+
+python ../../REA_with_CESM2/data_extractor/extract.py --realm atm --h_identifier h1 --variable TREFHT --preprocessing regional_average_TREFHT --project_path ../../REA_heat_wEU_JJA/ --experiment_identifiers $ID
+
+python ../../REA_with_CESM2/evaluate_ensemble.py --project_path ../../REA_heat_wEU_JJA/ --experiment_identifiers $ID
