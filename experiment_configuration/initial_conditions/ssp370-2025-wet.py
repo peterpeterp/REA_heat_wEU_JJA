44cde113ffcd339de8ccbc239ccd082a0ff7786f
Peter Pfleiderer <peter.pfleiderer@posteo.de>
2025-12-25 17:40:40 +0100

    now wet conditions are correct (not the same as dry anymore)

--- experiment_configuration/initial_conditions/ssp370-2025-wet.py
+++ experiment_configuration/initial_conditions/ssp370-2025-wet.py
@@ -12,16 +12,18 @@ class initial_condition_config():
 
         self.initial_conditions = []
 
-        wet_selection = ['/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens020.fE.2020.ens000/2024-01-01_to_2024-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.1200.fE.2020.ens000/2024-01-01_to_2024-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens009.fE.2020.ens000/2025-01-01_to_2025-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens016.fE.2020.ens000/2024-01-01_to_2024-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens018.fE.2020.ens000/2025-01-01_to_2025-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.1400.fE.2020.ens000/2024-01-01_to_2024-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens011.fE.2020.ens000/2026-01-01_to_2026-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens007.fE.2020.ens000/2026-01-01_to_2026-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.1000.fE.2020.ens000/2024-01-01_to_2024-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.0500.fE.2020.ens000/2024-01-01_to_2024-05-31']
+        wet_selection = [
+            '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.1300.fE.2020.ens000/2024-01-01_to_2024-05-31',
+            '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens022.fE.2020.ens000/2025-01-01_to_2025-05-31',
+            '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens009.fE.2020.ens000/2026-01-01_to_2026-05-31',
+            '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens020.fE.2020.ens000/2025-01-01_to_2025-05-31',
+            '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens012.fE.2020.ens000/2025-01-01_to_2025-05-31',
+            '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens008.fE.2020.ens000/2024-01-01_to_2024-05-31',
+            '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens013.fE.2020.ens000/2025-01-01_to_2025-05-31',
+            '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens001.fE.2020.ens000/2025-01-01_to_2025-05-31',
+            '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens024.fE.2020.ens000/2025-01-01_to_2025-05-31',
+            '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens005.fE.2020.ens000/2025-01-01_to_2025-05-31'
+        ]
 
         for sim in wet_selection:
             for i in range(10):
