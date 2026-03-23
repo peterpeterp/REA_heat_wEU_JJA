a6626c55c2e01839a96f9e9401b342bd2db194f5
Peter Pfleiderer <peter.pfleiderer@posteo.de>
2026-03-23 16:53:09 +0100

    new location of restart files

--- experiment_configuration/initial_conditions/ssp370-2025-dry.py
+++ experiment_configuration/initial_conditions/ssp370-2025-dry.py
@@ -12,16 +12,16 @@ class initial_condition_config():
 
         self.initial_conditions = []
 
-        dry_selection = ['/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens020.fE.2020.ens000/2024-01-01_to_2024-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.1200.fE.2020.ens000/2024-01-01_to_2024-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens009.fE.2020.ens000/2025-01-01_to_2025-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens016.fE.2020.ens000/2024-01-01_to_2024-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens018.fE.2020.ens000/2025-01-01_to_2025-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.1400.fE.2020.ens000/2024-01-01_to_2024-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens011.fE.2020.ens000/2026-01-01_to_2026-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens007.fE.2020.ens000/2026-01-01_to_2026-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.1000.fE.2020.ens000/2024-01-01_to_2024-05-31',
-       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.0500.fE.2020.ens000/2024-01-01_to_2024-05-31']
+        dry_selection = ['/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens020.fE.2020.ens000/branch2024-01-01_to_2024-05-31',
+       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.1200.fE.2020.ens000/branch2024-01-01_to_2024-05-31',
+       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens009.fE.2020.ens000/branch2025-01-01_to_2025-05-31',
+       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens016.fE.2020.ens000/branch2024-01-01_to_2024-05-31',
+       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens018.fE.2020.ens000/branch2025-01-01_to_2025-05-31',
+       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.1400.fE.2020.ens000/branch2024-01-01_to_2024-05-31',
+       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens011.fE.2020.ens000/branch2026-01-01_to_2026-05-31',
+       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.001.2005.ens007.fE.2020.ens000/branch2026-01-01_to_2026-05-31',
+       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.1000.fE.2020.ens000/branch2024-01-01_to_2024-05-31',
+       '/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.0500.fE.2020.ens000/branch2024-01-01_to_2024-05-31']
 
         for sim in dry_selection:
             for i in range(10):
--- experiment_configuration/initial_conditions/ssp370-2025.py
+++ experiment_configuration/initial_conditions/ssp370-2025.py
@@ -9,7 +9,7 @@ class initial_condition_config():
 
         # initial condition restart files
         self.initial_condition_fake_year = 2025
-        self.initial_conditions = sorted(glob.glob(f"/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.*.fE.2020.ens000/*-01-01_to_*-{start_date_in_year}"))
+        self.initial_conditions = sorted(glob.glob(f"/work/bb1445/u290372/cesm215_archive/BSSP370cmip6_current_2021-2031/b.e215.BSSP370cmip6.f09_g17.*.fE.2020.ens000/branch/*-01-01_to_*-{start_date_in_year}"))
 
         # number of members
         self.n_members = len(self.initial_conditions)
